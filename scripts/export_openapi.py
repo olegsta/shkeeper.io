6150f97a331b9cad5f474d9583d39ded87314e35
oleh starosvitskyi <oleg.starosvitskij@gmail.com>
2026-04-22 10:37:10 +0200

    fix

---
+++ scripts/export_openapi.py
@@ -0,0 +1,20 @@
+from shkeeper import create_app
+import json
+import os
+
+def main():
+    app = create_app()
+
+    with app.app_context():
+        api = app.extensions["smorest"]
+        spec = api.spec.to_dict()
+
+        os.makedirs("static/openapi", exist_ok=True)
+
+        with open("static/openapi.json", "w") as f:
+            json.dump(spec, f, indent=2)
+
+        print("OpenAPI exported")
+
+if __name__ == "__main__":
+    main()
